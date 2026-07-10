e0731926269797f11bb6ec843a51ff64ed98e295
yssahclemente <allyssah_clemente@dlsu.edu.ph>
2026-07-10 19:12:33 +0800

    modified: ItemSlot.java

--- ItemSlot.java
+++ ItemSlot.java
@@ -1,3 +1,36 @@
 public class ItemSlot {
-    int slotNumber
+
+    private final int slotNumber;
+    private Item item;
+    private int quantity;
+    private final int capacity = 10;
+
+    public ItemSlot(int slotNumber, Item item, int quantity) {
+        this.slotNumber = slotNumber;
+        this.item = item;
+        
+        if (quantity < 0 || quantity > capacity) {
+            throw new IllegalArgumentException("Invalid quantity.");
+        }
+
+        this.quantity = quantity;
+    }
+
+    public boolean isAvailable() {
+        return quantity > 0;
+    }
+
+    public void dispenseItem() {
+        if (isAvailable()) {
+            quantity--;
+        }
+    }
+
+    // public void restock() {
+
+    // }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
 }
